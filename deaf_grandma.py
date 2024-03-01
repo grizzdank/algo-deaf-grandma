df24024f166ad0d289fe725c6b21a0881cd9bd0d
grizzdank <dave.e.graham@gmail.com>
2024-03-01 10:18:56 -0800

    Python draft

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,25 @@
+# Imitates a grandma who is hard of hearing
+# Author: Dave Graham
+# Date: 03012024
+
 def deaf_grandma():
-
-
+    bye = 0
+    print('HEY KID!')
+    while bye < 2:
+        kid = input('>')
+        if kid == 'GOODBYE!':
+            bye += 1
+            if bye == 1:
+                print('LEAVING SO SOON?')
+                continue
+            elif bye == 2:
+                print('LATER, SKATER!')
+                exit()
+        if str.isupper(kid):
+            print('NO, NOT SINCE 1946!')
+        elif not kid: 
+            print('WHAT?!')
+        else:
+            print('SPEAK UP, KID!')
 
 deaf_grandma()
